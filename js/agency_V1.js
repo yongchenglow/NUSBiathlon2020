c92fa971e756a83d67bd4c0a8c42402f72630c4d
Yong Cheng Low <lowyongcheng@hotmail.com>
2020-01-02 13:03:52 +0800

    adjusted Navbar and carousel to make it responsive

--- js/agency_V1.js
+++ js/agency_V1.js
@@ -26,7 +26,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top - 54)
+          scrollTop: (target.offset().top - 58)
         }, 1000, "easeInOutExpo");
         return false;
       }
@@ -41,7 +41,7 @@
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#mainNav',
-    offset: 56
+    offset: 58
   });
 
   // Collapse Navbar
